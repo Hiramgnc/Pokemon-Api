dcde5a5d7fe4ed89ec89f4a2cac00f4a7fa465a0
Hiramgnc <hiramgnc@hotmail.com>
2022-09-02 21:50:57 -0300

    2 commit

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,66 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+      },
+
+    //Nombre 
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: "https://i.postimg.cc/ydrGgdVx/default.webp"
+    },
+
+    //Vida
+    hp: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    //Ataque
+    attack: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    //Defensa
+    defense: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    //Velocidad
+    speed: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    //Altura
+    height: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    //Peso
+    weight: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    createInDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+
+  }, {timestamps: false});
 };
