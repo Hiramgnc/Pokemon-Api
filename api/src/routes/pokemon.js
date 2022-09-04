5a2751b4302872eba8af4819cb50fb856c2a6adf
Hiramgnc <hiramgnc@hotmail.com>
2022-09-04 02:11:07 -0300

    4 commit

--- api/src/routes/pokemon.js
+++ api/src/routes/pokemon.js
@@ -11,38 +11,35 @@ const router = Router();
 // Ejemplo: router.use('/auth', authRouter);
 
 //https://pokeapi.co/api/v2/pokemon/1
-const getApiInfo = async () =>{
 
-    try{
-        const apiUrl = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=40')
-            const result = apiUrl.data.results.map(e=>  axios.get(e.url))
-                //Result es un array con los resultados de la segunda consulta
-                let apiInfo = Promise.all(result) //Guardo la promesa para retornarla
-                    //Este e es un array con los objetos data de la respuesta axios 
-                    .then(e=> {
-                        let pokemon = e.map(e=> e.data) //Crea un array con los objetos pokemon
-                        let arrPokemons = []
-                        pokemon.map(e => {
-                            arrPokemons.push({
-                                id: e.id,
-                                name : e.name,
-                                image: e.sprites.other.dream_world.front_default,
-                                hp: e.stats[0].base_stat,
-                                attack: e.stats[1].base_stat,
-                                defense: e.stats[2].base_stat,
-                                speed: e.stats[5].base_stat,
-                                height: e.height,
-                                weight: e.weight,
-                                })
-                        })
-                        return arrPokemons
-                    })
-                return apiInfo
+const getApiInfo = async () => {
 
-    }catch(err){
-        console.log(err)
+    const apiUrl = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=40");
+    const apiInfo = await apiUrl.data.results;
+    let allPokemons = [];
+
+    for (let i = 0; i < apiInfo.length; i++) {
+
+        await axios.get(apiInfo[i].url).then((e) =>
+
+            allPokemons.push({
+                id: e.data.id,
+                name: e.data.name,
+                img: e.data.sprites.other.home.front_default,
+                hp: e.data.stats[0].base_stat,
+                attack: e.data.stats[1].base_stat,
+                defense: e.data.stats[2].base_stat,
+                speed: e.data.stats[5].base_stat,
+                height: e.data.height,
+                weight: e.data.weight,
+                types: e.data.types.map((e) => e.type),
+
+            })
+        );
     }
-}    
+    
+    return allPokemons;
+};
 
 
 const getDbInfo = async () => {
